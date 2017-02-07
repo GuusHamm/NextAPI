a3e09a40b946b371b6c5acb0f5c51db1403780d1
Guus Hamm <guushamm@gmail.com>
2017-02-07 14:20:24 +0100

    added location admin

--- location/admin.py
+++ location/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from location.models import Location
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('lat', 'long', 'pcn', 'timestamp')
+    list_filter = ('pcn', 'timestamp')
+
+admin.site.register(Location, LocationAdmin)
