0ed38cb66eff1fa704164c8cad4fdcdbcb8f7dd8
Guus Hamm <guushamm@gmail.com>
2017-02-07 21:16:20 +0100

    Add student admin

--- location/admin.py
+++ location/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from location.models import Location
+from location.models import Location, Student
 
 
 class LocationAdmin(admin.ModelAdmin):
     list_display = ('lat', 'long', 'pcn', 'timestamp')
     list_filter = ('pcn', 'timestamp')
 
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'givenName', 'mail')
+    search_fields = ['pcn', 'givenName']
+
+
 admin.site.register(Location, LocationAdmin)
+admin.site.register(Student, StudentAdmin)
